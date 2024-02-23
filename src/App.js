fd3da50f75da6afdc8fbd34fe4f65647ce4ec0d2
harsh Bopaliya <bopaliyaharsh7@gmail.com>
2024-02-23 20:06:28 +0530

    nottodo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./MyComponents/Header";
+import Footer from "./MyComponents/Footer";
+import About from "./MyComponents/About";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="My TodosList" searchBar={false} />
+      <About />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>hello</p>
+    </div>
+  );
+};
+
+export default About;
