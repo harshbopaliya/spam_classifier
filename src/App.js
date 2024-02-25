59abb7985ef00c40720f41e9691f197c5baee43a
harsh Bopaliya <bopaliyaharsh7@gmail.com>
2024-02-25 13:55:01 +0530

    cleanig

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import "./App.css";
-import Header from "./MyComponents/Header";
-import Footer from "./MyComponents/Footer";
-import About from "./MyComponents/About";
-import SpamClassifier from "./MyComponents/spamClassifier";
+import NavBar from "./COMPONENT/NavBar";
 
 function App() {
   return (
     <>
-      <Header title="My TodosList" searchBar={false} />
-      <SpamClassifier />
-      <About />
-      <Footer />
+      <NavBar />
     </>
   );
 }
